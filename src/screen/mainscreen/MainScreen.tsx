3d7a83b093320505a0d5ec7599cd5d301a04518e
Mr. Robot <mr.robot@Amits-MacBook-Air.local>
2023-12-11 02:59:39 +0530

    Adding the UI dependencies for creating the front-end ui.

---
+++ src/screen/mainscreen/MainScreen.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IMainScreenProps {
+}
+
+export default function MainScreen (props: IMainScreenProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
